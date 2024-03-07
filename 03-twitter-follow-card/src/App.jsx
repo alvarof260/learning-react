d4a929cee2a0a2b7ca7affd75195debfef6a2f2b
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-07 14:39:52 -0300

    feat: twitter follow card

--- 03-twitter-follow-card/src/App.jsx
+++ 03-twitter-follow-card/src/App.jsx
@@ -1,7 +1,21 @@
 function App() {
   return (
     <>
-      <h1 className=" text-cyan-600 text-center">hello word!</h1>
+      <main className="flex justify-center items-center h-screen">
+        <section className="bg-twitter-blue w-[348px] h-[302px] rounded-2xl">
+          <header className="px-3 py-2">
+            <h3 className=" text-twitter-lighter font-bold text-2xl">
+              Who follow
+            </h3>
+          </header>
+
+          <footer className="h-[54px] px-3 py-2 flex justify-start items-center">
+            <a href="#" className="text-twitter-blueLight text-sm">
+              Show More
+            </a>
+          </footer>
+        </section>
+      </main>
     </>
   );
 }
--- 03-twitter-follow-card/tailwind.config.js
+++ 03-twitter-follow-card/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#16181C",
+        "twitter-dark": "#000000",
+        "twitter-light": "#71767B",
+        "twitter-lighter": "#E7E9EA",
+        "twitter-gray": "#1D1F23",
+        "twitter-blueLight": "#1c93e4",
+      },
+    },
   },
   plugins: [],
-}
-
+};
