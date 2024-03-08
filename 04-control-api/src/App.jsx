270c94b22019829d5127a3661412e891986edcac
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-08 12:24:04 -0300

    Correcciones en la API de control

--- 04-control-api/src/App.jsx
+++ 04-control-api/src/App.jsx
@@ -8,21 +8,31 @@ function App() {
   const [image, setImage] = useState(null);
   useEffect(() => {
     fetch(FACTAPI_URL)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        response.json();
+      })
       .then((data) => {
         setFact(data.fact);
-        const word = data.fact.split(" ")[0];
-        fetch(`${IMAGEAPI_URL}/cat/says/${word}?size=50&color=red&json=true`)
-          .then((response) => response.json())
-          .then((data) => {
-            const { _id } = data;
-            const url = `${IMAGEAPI_URL}/cat/${_id}/says/${word}`;
-            setImage(url);
-          })
-          .catch((error) => console.error(error));
       })
       .catch((error) => console.error(error));
   }, []);
+
+  useEffect(() => {
+    if (!fact) return;
+    const word = fact.split(" ")[0];
+    fetch(`${IMAGEAPI_URL}/cat/says/${word}?size=50&color=red&json=true`)
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        response.json();
+      })
+      .then((data) => {
+        const { _id } = data;
+        const url = `${IMAGEAPI_URL}/cat/${_id}/says/${word}`;
+        setImage(url);
+      })
+      .catch((error) => console.error(error));
+  }, [fact]);
 
   return (
     <>
@@ -31,9 +41,9 @@ function App() {
           <h1 className="text-4xl font-bold text-fuchsia-600">Control API'S</h1>
         </header>
         <section className="flex flex-col justify-center items-center p-8 h-full gap-4">
-          <p className="text-slate-400 text-lg">{fact}</p>
+          {fact && <p className="text-slate-400 text-lg">{fact}</p>}
           <figure className=" max-w-96">
-            <img className="w-full" src={image} alt={fact} />
+            {image && <img className="w-full" src={image} alt={fact} />}
           </figure>
         </section>
       </main>
